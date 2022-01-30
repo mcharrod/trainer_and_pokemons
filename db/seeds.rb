2fa6378be4077f8ba59bd4286d460b0f6ec69271
Katy Harrod <m.catherine.harrod@gmail.com>
2022-01-30 16:44:30 -0700

    Order trainers by created at on index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,4 @@
 
 @ash = Trainer.find_or_create_by!(name: "Ash Ketchum", age: 10, certified: false)
 @blue = Trainer.find_or_create_by!(name: "Blue", age: 11, certified: false)
+@red = Trainer.find_or_create_by!(name: "Red", age: 12, certified: false)
